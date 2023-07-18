8d0ac05d9800c27af832e4e54abe94f971791ab2
amiralzd <123456amir78900@gmail.com>
2023-07-18 11:32:41 +0330

    responsive

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function navbar(){
-    var x = document.getElementById("mynav");
-    if(x.className === "nav"){
-        x.className += " responsive";
-    }else{
-        x.className = "nav";
-    }
+// function navbar(){
+//     var x = document.getElementById("mynav");
+//     if(x.className === "nav"){
+//         x.className += " responsive";
+//     }else{
+//         x.className = "nav";
+//     }
+// }
+
+
+function mynav(){
+    let x = document.getElementById("dropdown");
+    x.classList.toggle("navbar");
+}
+
+
+function mysearch(){
+    let y = document.getElementById("searchh");
+    y.classList.toggle("mysearchstyle");
 }
